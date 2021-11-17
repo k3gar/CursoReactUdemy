fef445a941b2461f057131540674f755ffe5b083
Kevin Garcia <kevinede40@gmail.com>
2021-11-16 21:59:02 -0600

    Small practice

--- crud-firestore/src/App.jsx
+++ crud-firestore/src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import {firebase} from './firebase'
 function App() {
+  const [tarea, setTarea] = React.useState([])
 
   React.useEffect(()=> {
+
 
     const obtenerDatos = async () => {
       try{
         const db = firebase.firestore()
         const data = await db.collection('tareas').get()
-        console.log(data.docs)
+        
+        const arrayData = data.docs.map(doc => ({id: doc.id, ...doc.data() }))
+        console.log(arrayData)
+        setTarea(arrayData)
+        
       }catch (error){
         console.log(error)
       }
@@ -18,8 +24,23 @@ function App() {
 
   },[])
   return (
-    <div className="container">
-      dfg
+    <div className="container mt-3">
+      <div className="row">
+        <div className="col-md-6">
+          <ul className="list-group">
+            {
+              tarea.map(item => (
+                <li className="list-group-item" key={item.id}>
+                  {item.name}
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+        <div className="col-md-6">
+          Formulario
+        </div>
+      </div>
       
     </div>
   );
