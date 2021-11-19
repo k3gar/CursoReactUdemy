e7b12738392f601943c1df0da5f04b6fdff6c857
Kevin Garcia <kevinede40@gmail.com>
2021-11-18 21:45:38 -0600

    Adding Add and Delete Functions

--- crud-firestore/src/App.jsx
+++ crud-firestore/src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import {firebase} from './firebase'
 function App() {
-  const [tarea, setTarea] = React.useState([])
+  const [tareas, setTareas] = React.useState([]);
+  const [tarea, setTarea] = React.useState("");
+  const [modoEdicion, setModoEdicion] = React.useState(false)
+  
 
   React.useEffect(()=> {
 
@@ -13,7 +16,7 @@ function App() {
         
         const arrayData = data.docs.map(doc => ({id: doc.id, ...doc.data() }))
         console.log(arrayData)
-        setTarea(arrayData)
+        setTareas(arrayData)
         
       }catch (error){
         console.log(error)
@@ -23,22 +26,78 @@ function App() {
     obtenerDatos()
 
   },[])
+
+  const agregar = async (e) => {
+    e.preventDefault();
+    if(!tarea.trim()){
+      console.log('Elemento vacío')
+      return
+    }
+    try {
+      const db = firebase.firestore()
+      const nuevaTarea = {
+          name: tarea,
+          fecha: Date.now()
+      }
+      const data = await db.collection('tareas').add({
+          name: tarea,
+          fecha: Date.now()
+      })
+      setTareas([
+          ...tareas,
+          {id: data.id, ...nuevaTarea }
+      ])
+      setTarea('')
+  } catch (error) {
+      console.log(error)
+  }
+
+    console.log(tarea)
+  }
+
+  const eliminar = async (id) => {
+    try {
+      const db = firebase.firestore();
+      await db.collection('tareas').doc(id).delete();
+
+      const arrayFiltrado = tareas.filter(item => item.id !== id);
+      setTareas(arrayFiltrado)
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className="container mt-3">
       <div className="row">
         <div className="col-md-6">
           <ul className="list-group">
             {
-              tarea.map(item => (
+              tareas.map(item => (
                 <li className="list-group-item" key={item.id}>
                   {item.name}
+                  <button className="btn btn-danger btn-sm float-right" onClick={() => eliminar(item.id)}>Eliminar</button>
+                  <button className="btn btn-warning btn-sm float-right ">Editar</button>
                 </li>
               ))
             }
           </ul>
         </div>
         <div className="col-md-6">
-          Formulario
+          <h3>
+            {modoEdicion ? 'Editar Tarea' : 'Agregar Tarea'}
+            </h3>
+          <form onSubmit={agregar}>
+            <input type="text" placeholder="Ingrese Tarea" className="form-control mb-2" value={tarea} onChange={e => setTarea(e.target.value)} />
+            <button className={
+              modoEdicion ? 'btn btn-warning btn-block' : 'btn btn-dark btn-block'
+            } type="submit">
+              {
+                modoEdicion ? 'Editar' : 'Agregar'
+              }
+            </button>
+          </form>
         </div>
       </div>
       
